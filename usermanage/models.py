4a0dc62598ee5e581841020eb4ddf542ebdc95d4
Krzysztof Dzienis <kdzienis@gmail.com>
2017-11-15 17:31:42 +0100

    fix for pep8

--- usermanage/models.py
+++ usermanage/models.py
@@ -2,6 +2,7 @@ import random
 
 from django.contrib.auth.models import AbstractUser, models
 
+
 class MyUser(AbstractUser):
     birthday = models.DateField(null=True)
-    randomNumber = models.IntegerField(default=random.randint(0,100))
+    randomNumber = models.IntegerField(default=random.randint(0, 100))
