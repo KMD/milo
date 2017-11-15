05eb772d17e4a723f21258ac8594393a89190e33
Krzysztof Dzienis <kdzienis@gmail.com>
2017-11-15 21:15:28 +0100

    add generic views

--- usermanage/views.py
+++ usermanage/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from usermanage.models import MyUser
 
 
 def index(request):
-    return render(request, "usermanage/index.html", {})
+    users = MyUser.objects.all()
+    context = {'users': users}
+    return render(request, "usermanage/index.html", context)
+
 
 def show_user(request, id):
-    return render(request, "usermanage/index.html", {})
+    my_user = MyUser.objects.get(id=id)
+    context = {'user': my_user}
+    return render(request, "usermanage/user.html", context)
 
 
 class MyUserCreate(CreateView):
     model = MyUser
-    fields = ['username','first_name', 'last_name', 'email', 'birthday']
+    fields = ['username', 'first_name', 'last_name', 'email', 'birthday']
+
+
+class MyUserDelete(DeleteView):
+    model = MyUser
+    success_url = '/'
+
+
+class MyUserUpdate(UpdateView):
+    model = MyUser
+    fields = ['username', 'first_name', 'last_name', 'email', 'birthday']
+    template_name_suffix = '_update_form'
